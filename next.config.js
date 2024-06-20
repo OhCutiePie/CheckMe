c4290420d23b6c54cd090f2921589ebd9d40faf3
froggy <49438518+OhCutiePie@users.noreply.github.com>
2024-06-20 02:32:28 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = { }
+const nextConfig = {
+  basePath: '/your-repo-name',
+  assetPrefix: '/your-repo-name/',
+};
 
 module.exports = nextConfig;
