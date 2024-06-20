e897940aba27cf6ac1f50dcaa2d77aa179880a9c
froggy <49438518+OhCutiePie@users.noreply.github.com>
2024-06-20 02:32:18 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,53 +1,47 @@
-'use client'
+'use client';
 
 import Link from 'next/link';
 import { useState } from 'react';
 
-export default function page()
-{
-
+export default function Page() {
   const [img, $img] = useState('puppy-eyes');
   const [how, $how] = useState(false);
   const [p, $p] = useState(null);
 
-  const updteImg = (im) =>
-  {
+  const updateImg = (im) => {
     $img(im);
-  }
+  };
 
-  const nah = () =>
-  {
+  const nah = () => {
     const x = Math.random() * 90;
     const y = Math.random() * 90;
 
     $p({ x: `${x}%`, y: `${y}%` });
-    updteImg('unamused');
+    updateImg('unamused');
   };
 
-  const dontdoit = () =>
-  {
+  const dontdoit = () => {
     nah();
-    updteImg('shock');
-    $how(true)
-  }
+    updateImg('shock');
+    $how(true);
+  };
 
   return (
-    <div className='box'>
-      <img alt='gumball' className='gif' src={`/${img}.gif`} />
-      {how ? 'Hmmmph, Theres no choice!' : 'Will you go out with me?'}
-      <div className='btns'>
-        <Link className='yes' href='/yay'>
-          <button onMouseOver={() => updteImg('sparkle-eyes')} onMouseOut={() => updteImg('puppy-eyes')}>
+    <div className="box">
+      <img alt="gumball" className="gif" src={`/${img}.gif`} />
+      {how ? "Hmmmph, Theres no choice!" : "Will you go out with me?"}
+      <div className="btns">
+        <Link className="yes" href="/yay">
+          <button onMouseOver={() => updateImg('sparkle-eyes')} onMouseOut={() => updateImg('puppy-eyes')}>
             Yes
           </button>
         </Link>
-        <button className='no'
+        <button className="no"
           style={p ? { position: 'absolute', top: p.x, right: p.y } : null}
           onClick={dontdoit} onMouseOver={nah}>
           No
         </button>
       </div>
     </div>
-  )
-
+  );
 }
